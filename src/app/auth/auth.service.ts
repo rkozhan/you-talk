5f7673bf756b3b697f020579baeb1399d262289c
rkozhan~ <yalta.vrk@gmail.com>
2024-10-22 21:27:00 +0200

    use RxJs in auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+import { TokenResponce } from './auth.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +10,26 @@ export class AuthService {
   http = inject(HttpClient)
   baseApiUrl = 'https://icherniakov.ru/yt-course/auth/'
 
+  token: string | null = null
+  refreshToken: string | null = null
+
+  get isAuth() {
+    return !!this.token
+  }
+
   login(payload: {username: string, password: string}) {
     const fd = new FormData()
     fd.append('username', payload.username)
     fd.append('password', payload.password)
-    return this.http.post(
+
+    return this.http.post<TokenResponce>(
       `${this.baseApiUrl}token`,
       fd
+    ).pipe(
+      tap(val => {
+        this.token = val.access_token
+        this.refreshToken = val.refresh_token
+      })
     )
   }
 
