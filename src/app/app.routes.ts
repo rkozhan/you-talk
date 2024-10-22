00ec6961e20fde1a5cf4ab710030f20ae8c5e55d
rkozhan~ <yalta.vrk@gmail.com>
2024-10-22 10:17:03 +0200

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './common-ui/layout/layout.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginPageComponent},
+    {path: '', component: LayoutComponent, children: [
+        {path: 'profile', component: ProfilePageComponent},
+        {path: '', component: SearchPageComponent}
+    ]},
+];
+
--- src/app/data/helpers/pipes/img-url.pipe.ts
+++ src/app/data/helpers/pipes/img-url.pipe.ts
@@ -6,8 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ImgUrlPipe implements PipeTransform {
 
-  transform(value: string | null): string | null {
-    //if (!value) return null;
+  transform(value: string | null): string {
     if (!value) return '/assets/imgs/avatar.png';
     return `https://icherniakov.ru/yt-course/${value}`;
   }
