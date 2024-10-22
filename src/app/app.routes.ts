19d381d413851ed7916454a6b8b22a0a8ac26f16
rkozhan~ <yalta.vrk@gmail.com>
2024-10-22 16:29:11 +0200

    add auth service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,10 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 export const routes: Routes = [
-    {path: 'login', component: LoginPageComponent},
     {path: '', component: LayoutComponent, children: [
-        {path: 'profile', component: ProfilePageComponent},
-        {path: '', component: SearchPageComponent}
+        {path: '', component: SearchPageComponent},
+        {path: 'profile', component: ProfilePageComponent}
     ]},
+    {path: 'login', component: LoginPageComponent}
 ];
 
